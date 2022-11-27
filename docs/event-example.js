a6524c1527c2860509cdbed16166965135516139
Yasukazu Makino <mkn@yskz.dev>
2022-11-27 14:43:39 +0900

    Change Nav height

--- docs/event-example.js
+++ docs/event-example.js
@@ -10,26 +10,28 @@ userLanguage_docObj.textContent = `${userLanguage}`;
 const nav_expand_button = document.querySelector("button#nav-expand");
 const nav_h_str = '--nav-h';
 const num_unit_re = /\s*(\d+)(\w*)/;
-const expanded_nav_h = 80;
+const expanded_nav_h = 50;
 const expanded_unit = 'vh';
 let expanded = false;
-let nav_h = 0;
+let nav_h_val = 0;
+let nav_h_unit = '';
 nav_expand_button.onclick = () => {
   if (expanded) {
+    let new_nav_h_str = nav_h_val + nav_h_unit;
+    document.querySelector('body').style.setProperty(nav_h_str, new_nav_h_str);
+    expanded = false;
     return;
   }
   const cbody = getComputedStyle(document.querySelector('body'));
   nav_h = cbody.getPropertyValue(nav_h_str);
   let num_unit = num_unit_re.exec(nav_h);
-  let num = 0;
-  let unit = '';
   if (num_unit.length > 2) {
-    num = parseInt(num_unit[1]);
-    unit = num_unit[2];
+    nav_h_val = parseInt(num_unit[1]);
+    nav_h_unit = num_unit[2];
   }
   let new_nav_h_str = expanded_nav_h + expanded_unit;
   document.querySelector('body').style.setProperty(nav_h_str, new_nav_h_str);
-debugger;
+  expanded = true;
 }
 const event_divObj = document.querySelector('#event-div');
 const event_org_txt = event_divObj.textContent;
