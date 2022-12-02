88e9344d59875bad4a5464e24070e6a460aea16c
Yasukazu Makino <mkn@yskz.dev>
2022-12-02 18:18:36 +0900

    addEventListener

--- docs/nav-button.js
+++ docs/nav-button.js
@@ -1,14 +1,6 @@
-const cbody = getComputedStyle(document.querySelector('body'));
-const nav_h = cbody.getPropertyValue("--nav-h");
-const nav_h_2 = cbody.getPropertyValue("--nav-h-2");
-const nav_w = cbody.getPropertyValue("--nav-w");
-const nav_w_2 = cbody.getPropertyValue("--nav-w-2");
 const nav_on_off_button = document.querySelector("#nav-on-off");
 const nav_bar = document.querySelector("#nav-bar");
-debugger;
-const pc_media = "(min-width: 540px)";
-const dbody = document.querySelector('body').style;
-let navOn = false;
+
 nav_on_off_button.onclick = () => {
   if (nav_bar.style.display == 'block') {
     nav_bar.style.display = 'none';
@@ -16,3 +8,21 @@ nav_on_off_button.onclick = () => {
     nav_bar.style.display = 'block';
   }
 }
+
+const pc_media_query = window.matchMedia("(min-width: 540px)");
+
+function mediaChangeCheck(media_query) {
+  if (media_query.matches) {
+    nav_bar.style.display = 'block';
+    console.log("nav_bar display: block");
+  }
+}
+
+if ('addEventListener' in pc_media_query) {
+  debugger;
+  pc_media_query.addEventListener('change', mediaChangeCheck);
+} else {
+  console.log("Event listener is not added!");
+}
+
+mediaChangeCheck(pc_media_query);
